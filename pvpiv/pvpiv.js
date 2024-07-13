24660b76725a073a92553ec3070ac391694a02db
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2024-07-13 16:37:26 +0200

    Update pvpiv.js

--- pvpiv/pvpiv.js
+++ pvpiv/pvpiv.js
@@ -11,14 +11,14 @@ function calc() {
     rstats.reverse()
   
     for ( i in rstats ) {
-      stat[i] = document.getElementById(rstats[i]).value + baseStats[pokemon][i];
+      stat[i] = parseFloat(document.getElementById(rstats[i]).value) + baseStats[pokemon][i];
     }
 
     const lvl = parseInt(document.getElementById("lvl").value);
     alert(lvl);
     const cpm = cpms[lvl*2-2];
     alert(cpm);
-    const _0 = ((stat[0] * Math.sqrt(stat[1]) * Math.sqrt(stat[2]) / 10) * (cpm**2);
+    const _0 = ((stat[0] * Math.sqrt(stat[1]) * Math.sqrt(stat[2])) / 10) * (cpm**2);
     document.getElementById("result").innerHTML = "Calculated CP: " + _0;
     alert(_0);
   } catch (x) {
