af9ecfcede4a41afa8eec1cd85c839954c9566b0
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2024-07-13 15:55:55 +0200

    Update pvpiv.js

--- pvpiv/pvpiv.js
+++ pvpiv/pvpiv.js
@@ -14,7 +14,7 @@ function calc() {
     stat[i] = document.getElementById(rstats[i]).value + baseStats[pokemon][i];
   }
 
-  const lvl = document.getElementById("lvl").value;
+  const lvl = parseFloat(document.getElementById("lvl").value);
   const cpm = cpms[lvl*2-2];
   const _0 = ((stat[0] * Math.sqrt(stat[1]) * Math.sqrt(stat[2]) / 10) * (cpm**2);
   document.getElementById("result").innerHTML = _0;
