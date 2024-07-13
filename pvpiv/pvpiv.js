028f6ed703de7f1c8d032006c3ff465a6c963c6e
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2024-07-13 15:25:10 +0200

    Update pvpiv.js

--- pvpiv/pvpiv.js
+++ pvpiv/pvpiv.js
@@ -4,4 +4,13 @@ function calc() {
     0.0,
     0.0
   ]
+
+  const rstats = stats;
+  rstats.reverse()
+  
+  for ( i in rstats ) {
+    stat[i] = document.getElementById(rstats[i]).value;
+  }
+
+  alert(stat[0]+ " " + stat[1] + " " + stat[2]);
 }
