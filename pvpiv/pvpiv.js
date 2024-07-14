88cb3993c186ec666b79dcead243be31129473ef
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2024-07-14 15:40:18 +0200

    Update pvpiv.js

--- pvpiv/pvpiv.js
+++ pvpiv/pvpiv.js
@@ -1,22 +1,20 @@
 function calc() {
-  try {
-    const pokemon = document.getElementById("pokemon").value;
-  
-    const stat = [
-      0.0,
-      0.0,
-      0.0
-    ]
-  
-    for ( i in ["sta", "atk", "def"] ) {
-      stat[i] = parseFloat(document.getElementById(rstats[i]).value) + baseStats[pokemon][i];
-    }
+  const pokemon = document.getElementById("pokemon").value;
 
-    const lvl = parseInt(document.getElementById("lvl").value);
-    const cpm = cpms[lvl*2-2];
-    const _0 = ((Math.sqrt(stat[0]) * stat[1] * Math.sqrt(stat[2])) / 10) * (cpm**2);
-    document.getElementById("result").innerHTML = "Calculated CP: " + _0;
-  } catch (x) {
-    alert(x);
+  const stat = [
+    0.0,
+    0.0,
+    0.0
+  ]
+
+  const stats = ["sta", "atk", "def"]
+
+  for ( i in stats ) {
+    stat[i] = parseFloat(document.getElementById(stats[i]).value) + baseStats[pokemon][i];
   }
+
+  const lvl = parseInt(document.getElementById("lvl").value);
+  const cpm = cpms[lvl*2-2];
+  const _0 = ((Math.sqrt(stat[0]) * stat[1] * Math.sqrt(stat[2])) / 10) * (cpm**2);
+  document.getElementById("result").innerHTML = "Calculated CP: " + _0;
 }
