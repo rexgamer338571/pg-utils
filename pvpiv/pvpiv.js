6b5aa775b429655e81623558b1a63c1d4d059d7d
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2024-07-14 12:12:33 +0200

    Update pvpiv.js

--- pvpiv/pvpiv.js
+++ pvpiv/pvpiv.js
@@ -15,12 +15,9 @@ function calc() {
     }
 
     const lvl = parseInt(document.getElementById("lvl").value);
-    alert(lvl);
     const cpm = cpms[lvl*2-2];
-    alert(cpm);
-    const _0 = ((stat[0] * Math.sqrt(stat[1]) * Math.sqrt(stat[2])) / 10) * (cpm**2);
+    const _0 = ((Math.sqrt(stat[0]) * stat[1] * Math.sqrt(stat[2])) / 10) * (cpm**2);
     document.getElementById("result").innerHTML = "Calculated CP: " + _0;
-    alert(_0);
   } catch (x) {
     alert(x);
   }
